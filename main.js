7f9deccb2f5f201db7f33cd8f4e0dba6106cdc4e
Paolo Razon <plgrazon@gmail.com>
2018-08-07 16:44:54 -0700

    [ops] - Add idcMain to capture the input value and pass it to the main DOM.

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const {app, BrowserWindow, Menu} = electron;
+const {app, BrowserWindow, Menu, ipcMain} = electron;
 
 let mainWindow;
 let addWindow;
@@ -47,6 +47,12 @@ const createAddWindow = () => {
     addWindow = null;
   });
 }
+
+// Catch item:add
+ipcMain.on('item:add', (event, item) => {
+  mainWindow.webContents.send('item:add', item);
+  addWindow.close();
+});
 
 // Create menu template
 const mainMenuTemplate = [
