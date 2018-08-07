cb08dd8c047febe16ad5463853efe8e8ee67c9a6
Paolo Razon <plgrazon@gmail.com>
2018-08-07 11:42:08 -0700

    [ops] - Add menu and submenus with functionality.

--- main.js
+++ main.js
@@ -16,4 +16,32 @@ app.on('ready', () => {
     protocol: 'file:',
     slashes: true
   }));
+
+  // Build menu from template
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  // Insert menu
+  Menu.setApplicationMenu(mainMenu);
 });
+
+// Create menu template
+const mainMenuTemplate = [
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'Add Item',
+      },
+      {
+        label: 'Clear Items'
+      },
+      {
+        label: 'Quit',
+        accelerator: process.platform == 'darwin' ? 'Command+Q' :
+        'Ctrl+Q',
+        click() {
+          app.quit();
+        }
+      }
+    ]
+  }
+];
