cd820fa7f056c798dbc1cfe77aa1973530fcac28
Paolo Razon <plgrazon@gmail.com>
2018-08-07 17:06:48 -0700

    [ops] - Implement remove and clear functionality of the app.

--- main.js
+++ main.js
@@ -66,7 +66,10 @@ const mainMenuTemplate = [
         }
       },
       {
-        label: 'Clear Items'
+        label: 'Clear Items',
+        click() {
+          mainWindow.send('item:clear');
+        }
       },
       {
         label: 'Quit',
